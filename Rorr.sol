9dc998eaefa7a122e92bcc0016c2354e24b33c54
Nilotpal Roy <104548852+roy7909@users.noreply.github.com>
2023-07-11 14:09:08 +0530

    Update Rorr.sol

--- Rorr.sol
+++ Rorr.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
+contract Error {
+    
+    uint public Alice=0;
+
+    //Cannot withdraw more than we have
+    function withdraw(uint _i) public   {
+        require(Alice > _i, "Alice does not have enough money");
+        Alice-=_i;
+    }
+
+    //Cannot deposit if it results in valur greater than 100
+    function deposit(uint _i) public   {
+        Alice+=_i;
+        if (Alice > 100) {
+            revert("Alice can not have more than 100");
+        }
+    }
+
+    //Check is Alice has value 0
+    function isempty() public view returns (string memory){
+        assert(Alice == 0);
+        return "Yes, Alice has no money";
+    }
+
+}
